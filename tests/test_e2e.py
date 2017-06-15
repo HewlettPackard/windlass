bdb25dc358903bd63ea91fbde72a1ab9874975d3
Antoni Dabek <antoni.dabek@hpe.com>
2017-06-15 11:09:37 +0100

    Extra tests for image building in windlass
    This adds more realistic tests with images coming
    from multiple scenarious.
    
    Change-Id: I48bf6a8d3562d1271acf317ccb426c8bfc4e0ed6

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -16,17 +16,21 @@
 
 import docker
 import windlass
+from git import Repo
 import os
 from requests import get
+from shutil import copytree
+from tempfile import TemporaryDirectory
 import test.support
 import testtools
+from testtools.matchers import Contains
 from testtools.matchers import Equals
 
 
-class Test_E2E(testtools.TestCase):
+class FakeRegistry(testtools.TestCase):
 
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(self):
         self.registry_port = test.support.find_unused_port()
         self.client = docker.from_env(version='auto')
         self.registry = self.client.containers.run(
@@ -34,10 +38,13 @@ class Test_E2E(testtools.TestCase):
             detach=True,
             ports={'5000/tcp': self.registry_port})
 
-    def tearDown(self):
-        super().tearDown()
+    @classmethod
+    def tearDownClass(self):
         self.registry.kill()
         self.registry.remove()
+
+
+class Test_E2E_SelfBuild(FakeRegistry):
 
     def test_tags_in_registry(self):
         base = os.path.dirname(os.path.dirname(__file__))
@@ -61,3 +68,46 @@ class Test_E2E(testtools.TestCase):
                 self.registry_port))
         self.assertThat(response.status_code, Equals(200))
         self.assertThat(response.json()['name'], Equals('zing/windlass'))
+
+
+class Test_E2E_FakeRepo(FakeRegistry):
+
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass()
+        self.tempdir = TemporaryDirectory()
+        self.repodir = os.path.join(self.tempdir.name, 'fakerepo')
+        copytree('./tests/fakerepo', self.repodir)
+        self.repo = Repo.init(self.repodir)
+        self.repo.git.add('-A')
+        self.commitid = self.repo.index.commit('Commit 1').hexsha
+
+    def test_fake_repo(self):
+        self.client.containers.run(
+            'zing/windlass:latest',
+            '--debug --directory %s --repository 127.0.0.1:%d test' % (
+                self.repodir, self.registry_port,
+                ),
+            remove=True,
+            volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock'},
+                     self.repodir: {'bind': self.repodir}},
+            environment=windlass.load_proxy(),
+            )
+        self.check_proper_image_build('partial')
+        self.check_proper_image_build('full')
+
+    def check_proper_image_build(self, imagename):
+        image = self.client.images.get('127.0.0.1:%s/fakerepo%s' %
+                                       (self.registry_port, imagename))
+        _, tags = zip(*(t.split('/')[-1].split(':') for t in image.tags))
+        self.expectThat(tags, Contains('latest'),
+                        '%s image missing latest tag' % imagename)
+        self.expectThat(tags, Contains('branch_master'),
+                        '%s image missing branch tag' % imagename)
+        self.expectThat(tags, Contains('ref_' + self.commitid),
+                        '%s image missing commit tag')
+        self.expectThat(self.client.containers.run(image,
+                                                   remove=True).decode(),
+                        Equals(open('tests/fakerepo/%scontext/image_content.'
+                                    'txt' % imagename).read()),
+                        '%s image build is not valid')
