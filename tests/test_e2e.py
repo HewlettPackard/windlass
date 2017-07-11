3e8c59cfba5d94dbbff3542c0c275f94d8aab22f
Michael Kerrin <michael.kerrin@hpe.com>
2017-07-11 14:23:45 +0100

    Remove cipublish.py script
    The logic for this is in zing/publishing and will
    not be distributed with windlass if/when it gets released
    as open source
    
    Change-Id: Icdb2315733e30c3881b1207960d46ce3cfb8d414

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -15,7 +15,6 @@
 #
 
 import docker
-from windlass.cipublish import get_commit
 import windlass.images
 from git import Repo
 import os
@@ -110,21 +109,3 @@ class Test_E2E_FakeRepo(FakeRegistry):
         self.assertThat(response.status_code, Equals(200))
         self.assertThat(
             response.json()['name'], Equals('fakerepo%s' % imagename))
-
-
-class Test_CI_Publishing(FakeRegistry):
-
-    def test_ci_publish(self):
-        cmd = 'cipublish --docker-repo 127.0.0.1:%s ./products/dev.yml'
-        os.system(cmd % self.registry_port)
-        response = get(self.registry_url + '/v2/zing/windlass/tags/list')
-        self.assertThat(response.status_code, Equals(200))
-        respjson = response.json()
-        self.assertThat(respjson['name'], Equals('zing/windlass'))
-        self.expectThat(respjson['tags'],
-                        Equals([get_commit('.')]),
-                        'Too many tags:' + ','.join(respjson['tags']))
-        catalog = get(self.registry_url + '/v2/_catalog').json()
-        self.expectThat(catalog['repositories'],
-                        Equals(['zing/windlass']),
-                        'Too many images:' + ','.join(catalog['repositories']))
