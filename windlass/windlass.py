49e2592dbbe9338841fc7581e3c0d0c4c61cee69
Michael Kerrin <michael.kerrin@hpe.com>
2017-07-12 10:35:24 +0100

    Remove wait_for_registry
    While we mostly work with docker container registries
    this implementation doesn't work with Artifactory due to
    slightly differing API and the logic for handling this
    should be application specific. Otherwise this isn't
    worth keeping.
    
    Change-Id: Ib25929747c49024cf2ff4c974ade8b8abcd2c6be

--- windlass/windlass.py
+++ windlass/windlass.py
@@ -21,49 +21,7 @@ from windlass.products import Products
 
 
 from argparse import ArgumentParser
-import datetime
 import logging
-from multiprocessing import Process
-import time
-
-from docker.errors import APIError
-from docker.errors import NotFound
-from docker import from_env
-
-
-def wait_for_registry(ns, docker_image_registry, wait=600):
-    docker = from_env(version='auto')
-    timeout = datetime.datetime.now() + datetime.timedelta(seconds=wait)
-    while True:
-        if datetime.datetime.now() > timeout:
-            ns.failure_occured.set()
-            logging.exception(
-                'wait_for_registry %s timeout after %d seconds' % (
-                    docker_image_registry, wait))
-            return
-
-        try:
-            result = docker.api.pull(docker_image_registry + 'noimage')
-            # result is a simple string of the json response, check if either
-            # the requested image was found, or explicitly "not found" to
-            # determine if the docker deamon can reach the desired registry
-            if ("Digest: sha256" in result or
-                    "Error: image noimage not found" in result):
-                ns.registry_ready.set()
-                logging.info('wait_for_registry: registry detected')
-                return
-
-            time.sleep(1)
-            continue
-        except (APIError, NotFound):
-            pass
-        except Exception:
-            logging.exception('wait_for_registry: Failed')
-            ns.failure_occured.set()
-            return
-        finally:
-            if ns.failure_occured.wait(0.1):
-                return
 
 
 def main():
@@ -104,21 +62,11 @@ def main():
         parser.error(
             "--build-only and --push-only can't be specified at the same time")
 
-    if ns.docker_image_registry and \
-       not ns.docker_image_registry.endswith('/'):
-        ns.docker_image_registry = ns.docker_image_registry + '/'
-
     products = Products(products_to_parse=ns.products)
     images, charts = products.images, products.charts
 
     g = windlass.api.Windlass(images)
     g.setupLogging(ns.debug)
-
-    if not ns.build_only:
-        waitproc = Process(target=wait_for_registry,
-                           args=(g, ns.docker_image_registry),
-                           name='wait_for_registry')
-        waitproc.start()
 
     for chart_def in charts:
         process_chart(chart_def, ns)
