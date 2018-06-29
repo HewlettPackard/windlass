0e7b4167480f49254342277281d74bc0111d7fb2
Manuel Desbonnet <manuel.desbonnet@hpe.com>
2018-06-29 13:55:39 +0100

    Defer boto ecr client creation in FakeECRConnector
    Having the FakeECRConnector initialise the boto ecr client at creation
    time causes multiprocessing failures with pickling.
    
    Seen in change I1f3736bcec82b5df71922850ad9a9124af3a2edb which adds a
    test which uses the FakeECRConnector with windlass.api.upload().
    
    Follow the same mechanism used by the base class in using get_ecrc()
    
    Change-Id: I27acae5792c9ec97e5f03d5715efbc0af0929009

--- windlass/testing.py
+++ windlass/testing.py
@@ -21,7 +21,6 @@ Test support code for modules that use windlass
 # modules (e.g. remotes.py)?
 
 import base64
-import boto3
 import botocore.stub
 import contextlib
 import windlass.remotes
@@ -39,12 +38,14 @@ class FakeECRConnector(windlass.remotes.ECRConnector):
         self._aws_urn = (
             '%s.dkr.ecr.%s.amazonaws.com' % ('012345678901', 'test-region')
         )
-        ecrc = boto3.client(
-            'ecr', aws_access_key_id='None', aws_secret_access_key='None',
-            region_name='None',
+        policy_stub = '{ "Statement": [{"Sid": "zing"}]}'
+        super().__init__(
+            creds, path_prefixes, repo_policy or policy_stub
         )
-        self._stubber = botocore.stub.Stubber(ecrc)
-        self._stubber.activate()
+
+    def _init_stubber(self, client):
+        stubber = botocore.stub.Stubber(client)
+        stubber.activate()
 
         auth_resp = {'authorizationData': [{
             'proxyEndpoint': 'https://%s' % self._aws_urn,
@@ -52,11 +53,13 @@ class FakeECRConnector(windlass.remotes.ECRConnector):
                 b'test_username:test_password'
             ).decode('utf-8'),
         }]}
-        policy_stub = '{ "Statement": [{"Sid": "zing"}]}'
-        self._stubber.add_response('get_authorization_token', auth_resp, {})
-        super().__init__(
-            creds, path_prefixes, repo_policy or policy_stub, ecrc
-        )
+        stubber.add_response('get_authorization_token', auth_resp, {})
+        return stubber
+
+    def get_ecrc(self):
+        client = super().get_ecrc()
+        self._init_stubber(client)
+        return client
 
     def upload(self, local_name, upload_name=None, upload_tag=None):
 
